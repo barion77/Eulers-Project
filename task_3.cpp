f019606b89dde981b04fea7539fde2afe3b26a8e
unknown <barion224@gmail.com>
2022-06-05 18:56:24 +0300

    task 3

---
+++ task_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+    long long int num = 600851475143;
+    int solution = 0;
+
+    for (int i = 2; i < sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            int count = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                    count++;
+            }
+            if (count == 2)
+            {
+                solution = i;
+            }
+        }
+    }
+
+    // Solution: 6857
+}
