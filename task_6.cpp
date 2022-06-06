4db0b834aca849c8455856e4966ad29ecd3794ac
unknown <barion224@gmail.com>
2022-06-06 22:00:22 +0300

    task 6

---
+++ task_6.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int solution = 0;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        a += pow(i, 2);
+        b += i;
+    }
+
+    solution = pow(b, 2) - a;
+
+    // Solution: 25164150
+}
