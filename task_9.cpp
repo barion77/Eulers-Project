47acb6297b10c815e7fec4f8f324ab00d1aefaef
unknown <barion224@gmail.com>
2022-06-07 12:41:44 +0300

    task 9

---
+++ task_9.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    int solution = 0;
+    int x = 1000;
+
+    for (int a = 1; a < x + 1; a++)
+    {
+        for (int b = a + 1; b < x + 1; b++)
+        {
+            int c = x - a - b;
+            if (pow(a, 2) + pow(b, 2) == pow(c, 2))
+                solution = a * b * c;
+        }
+    }
+
+    // Solution: 31875000
+}
